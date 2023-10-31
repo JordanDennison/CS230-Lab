67ee176a9b80aac1f953e9a3b2c37930c4e6d37b
JordanDennison <jordandennison1999@gmail.com>
2023-10-30 20:16:52 -0400

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
+
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800245753</p>
