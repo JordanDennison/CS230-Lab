55d724d264692cc6fb9edeaeb454393b01824f45
JordanDennison <jordandennison1999@gmail.com>
2023-10-30 20:18:05 -0400

    Test Change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800245753</p>
-      <p>Hi I am Jordan Dennison</p>
+      <p>Hi I am Jordan Dennison Test</p>
     </div>
   );
 }
